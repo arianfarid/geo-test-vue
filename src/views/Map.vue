5edf116377670d00b1fc71a45d7d9fd1ad96dc9f
arianfarid <arian.blank.farid@gmail.com>
2021-08-21 11:19:23 -0400

    added tile layer
    Will be cool to have button to change base.

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,6 +1,18 @@
 <template>
-  <l-map style="height:50vh">
+  <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]" style="height:50vh">
     <l-geo-json :geojson="geojson" :options="geojsonOptions" />
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      layer-type="base"
+      name="OpenStreetMap"
+      :max-zoom="10"
+    />
+    <l-tile-layer
+      url="https://s3.amazonaws.com/te512.safecast.org/{z}/{x}/{y}.png"
+      attribution="<a href='https://blog.safecast.org/about/'>SafeCast</a> (<a href='https://creativecommons.org/licenses/by-sa/3.0/'>CC-BY-SA</a>"
+      :min-zoom="5"
+      :max-zoom="7"
+    />
   </l-map>
 </template>
 
@@ -8,15 +20,17 @@
 // DON'T load Leaflet components here!
 // Its CSS is needed though, if not imported elsewhere in your application.
 import "leaflet/dist/leaflet.css"
-import { LMap, LGeoJson } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
 
 export default {
   components: {
     LMap,
     LGeoJson,
+    LTileLayer
   },
   data() {
     return {
+      zoom: 2,
       geojson: {
         type: "FeatureCollection",
         features: [
