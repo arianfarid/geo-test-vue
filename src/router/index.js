4c72ac7eda6e742726aa6eb4684ef436eb81d317
arianfarid <arian.blank.farid@gmail.com>
2021-08-24 19:14:37 -0400

    started Composition API refactor

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Map.vue')
+  },
+
+    {
+    path: '/mainmap',
+    name: 'mainmap',
+    meta: {
+      title: 'GeoCollector Test - Map'
+    },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/MainMap.vue')
   }
 ]
 
