115f756fad59124ca1532562cbb1099c2fb817e5
arianfarid <arian.blank.farid@gmail.com>
2021-08-25 21:24:09 -0400

    better nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/map">Map</router-link> |
-    <router-link to="/mainmap">Main Map</router-link>
+  
+  <div class="grid grid-rows-1 grid-flow-col justify-items-center">
+  <div class="flex space-x-1">
+
+  <div id="nav" class="flex-none bg-gray-300 p-2 mr-0.5 ml-0.5 mb-1 rounded-b-sm">
+    <router-link to="/" class="border-r p-4">Home</router-link>
+    <router-link to="/about" class="border-r p-4">About</router-link>
+    <router-link to="/map" class="border-r p-4">Map</router-link>
+    <router-link to="/mainmap" class="p-4">Main Map</router-link>
+  </div>
+
+  </div>
   </div>
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <router-view/>
