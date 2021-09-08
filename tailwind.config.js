1f91c6845551e59f353e99824dbd183010659ff9
arianfarid <arian.blank.farid@gmail.com>
2021-09-08 17:47:50 -0400

    fixed select option
    use a computed function to determine options available in character dropdown. These are filtered with javascript's  .some() inside a .filter() function on the character array. I put the 'character_selected' as the first option, so when it is 'blank' it shows with the correct name.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      ringWidth: ['hover', 'active', 'focus'],
+      ringColor: ['hover', 'active', 'focus'],
+    },
   },
   plugins: [],
 }
